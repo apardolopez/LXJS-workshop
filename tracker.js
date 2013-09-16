3d7e49b7fbb0c3e630dc383a0eb2ae46c81cb9f5
Alejandro Lopez <alopez@Alejandros-MacBook-Pro.local>
2013-09-16 18:14:51 +0200

    Quick first prototype of tracker.js

---
+++ tracker.js
@@ -0,0 +1,49 @@
+
+n (window, $) {
+
+	var img,
+		trackInProgress = false,
+		queue = [];
+
+	// Some random userID
+	window.userId = window.userId || parseInt(Math.random() * 10000, 10);
+
+	// Tracks people into our A/B test
+	window.trackTest = function(testId) {
+		// We are tracking test ids for now
+		if(!testId) return;
+
+		queue.push(testId);
+
+		if(!trackInProgress) {
+			// Set flag to prevent further tracking calls to happen
+			requestTrackingImg();
+		}
+	};
+
+	var requestTrackingImg = function() {
+		var url = '';
+
+		trackInProgress = true;
+
+		img = document.createElement('img');
+		
+		img.onload = function() {
+			// Clear queue
+			queue = [];
+			// Reset flag
+			trackInProgress = false;
+			
+		};
+		
+		img.onerror = function() {
+			// Reset flag
+			trackInProgress = false;
+			// Try again
+			requestTrackingImg();
+			// Some cleverer mechanism would prevent this from making many calls
+		}
+		
+	}	
+	
+})(window, jQuery);
