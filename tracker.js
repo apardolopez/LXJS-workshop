d1f12f84b6ced2ae2b15d7bcd9f4b17cacff5d20
apardolopez <apardolopez@gmail.com>
2013-09-30 18:11:24 +0200

    Definite version of basic framework for performing javascript tracking

--- tracker.js
+++ tracker.js
@@ -1,61 +1,81 @@
-// Extend local storage and page reload
+/*
+ * Proof of concept of framework to achieve A/B testing tracking based on Javascript
+ * LXJS 2012 Booking.com workshop
+ * https://github.com/apardolopez/LXJS-workshop
+ *
+ * Copyright Alejandro Pardo Lopez and other contributors
+ * Released under the MIT license
+ *
+ * Date: 01-10-2013
+ */
 
-(function (window) {
+(function (trackingSettings, window, document, undefined) {
 
 	var img,
+		settings = trackingSettings || {},
 		trackInProgress = false,
 		queue = [],
-		errorTries = 5;
+		queuePointer = 0,
+		lastRequestedQueuePointer = 0,
+		defaultMaxErrorTries = 5,
+		errorTries = settings.maxTries ? settings.maxTries : defaultMaxErrorTries,
+		frameworkNamespace = trackingSettings.namespace ? trackingSettings.namespace : 'userExpTracking',
+		trackingImgUrl = trackingSettings.imgUrl ? trackingSettings.imgUrl : 'http://q-ec.bstatic.com/images/misc_images/citymab_MAB_-2601889_02.jpg';
 
-	// Some random userID
-	window.userId = window.userId || parseInt(Math.random() * 10000, 10);
+	// Expose function that tracks users into our A/B test
+	window[frameworkNamespace] = {
+		trackTest: function(testData) {
+			// We are tracking test ids for now
+			if(!testData) return;
+	
+			// Push data into our queue and increment pointer
+			queue.push(JSON.stringify(testData));
+			queuePointer++;
 
-	// Tracks people into our A/B test
-	window.trackTest = function(testId) {
-		// We are tracking test ids for now
-		if(!testId) return;
-
-		queue.push(testId);
-
-		if(!trackInProgress) {
-			// Set flag to prevent further tracking calls to happen
-			requestTrackingImg();
+			// If we haven't requested tracking img, do so
+			// Otherwise, keep our data in the queue to avoid potential cookie overrides
+			if(!trackInProgress) {
+				requestTrackingImg();
+			}
 		}
 	};
 
 	var requestTrackingImg = function() {
-		var url = 'test.jpg';
+		var url = trackingImgUrl;
 
+		// Add all experiment data. Each experiment data is separated by "|" char
+		url += '?exp_data=' + queue.join('|');
+
+		// Flag to indicate whether we have to request an img or just queue the data until previous img has been loaded
 		trackInProgress = true;
 
 		img = document.createElement('img');
-		
+
+		lastRequestedQueuePointer = queuePointer;
 		img.onload = function() {
-			console.log('load');
-			// Clear queue
-			queue = [];
+			// Clear queue and localStorage data
+			queue = queue.slice(lastRequestedQueuePointer);
+			if(queue.length) {
+				requestTrackingImg();
+			}
+			
 			// Reset flag
 			trackInProgress = false;
-			
+			// Reset max error tries
+			errorTries = settings.maxTries ? settings.maxTries : defaultMaxErrorTries;
 		};
-		
+
+		// If img request fails, try until 
 		img.onerror = function() {
 			if(errorTries) {
 				errorTries--;
-				console.log('error');
 				// Reset flag
 				trackInProgress = false;
 				// Try again
 				requestTrackingImg();
 			}
-		}
+		};
 
 		img.src = url;
-		
-		
-		
-		
-		window.testImg = img;
-	}	
-	
-})(window);
+	};
+})({},window, document);
